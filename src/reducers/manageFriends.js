ab762f25d2ec75b335cf16655bb17dc1d827438f
Cloudymolecule <baires82@gmail.com>
2021-05-02 12:31:24 -0400

    Done.

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,2 +1,12 @@
-export function manageFriends(state, action){
+export function manageFriends(state = {friends: []}, action){
+    console.log(action.friend)
+    switch(action.type) {
+        case 'ADD_FRIEND':
+            return {...state, friends: [...state.friends, action.friend]}
+        case 'REMOVE_FRIEND':
+            const newState = state.friends.filter(friend => friend.id !== action.id)
+            return {...state, friends: newState}
+        default:
+            return state
+    }
 }
